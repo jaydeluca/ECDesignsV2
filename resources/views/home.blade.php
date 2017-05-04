edf42f1f0a3720c54ebe8a0ed046364ebd894606
Jay DeLuca <jaydeluca4@gmail.com>
2017-05-03 20:28:05 -0400

    home page - second sectoin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,22 @@
             </a>
         </div>
 
+    </div>
 
+    <div class="section div-2" id="guinness">
+
+        <div class="div-2--content">
+            <p>
+                Guinness<br>
+                By The Numbers
+            </p>
+            <a class="c-btn">
+                View Infographic
+            </a>
+        </div>
 
     </div>
+
 </div>
 
 @include('layouts.footer')

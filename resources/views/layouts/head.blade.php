075211c2db0d3e4872d776f0b35bf17652064a75
Jay DeLuca <jaydeluca4@gmail.com>
2017-06-27 21:07:30 -0400

    meta tags

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Graphic Designer in the Boston, MA / Providence, RI area" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Eric Silva Design') }}</title>
+    <title>Eric Silva Design</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
